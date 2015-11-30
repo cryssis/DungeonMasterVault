56712694ee679ab6d19e79a1d4fd7ccb45d6e2d0
cryssis <roberto.sobreviela@gmail.com>
2015-11-30 13:07:47 +0100

    Implemented DataService

---
+++ DungeonMasterVault.Services/DataServices/SampleDataService.cs
@@ -0,0 +1,69 @@
+﻿namespace DungeonMasterVault.Services.DataServices
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using DungeonMasterVault.Core.Encounters;
+
+    /// <summary>
+    /// A class for DesignTime data
+    /// </summary>
+    public class SampleDataService : IDataService
+    {
+        private List<Encounter> encounters;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SampleDataService"/> class.
+        /// </summary>
+        public SampleDataService()
+        {
+            InitEncounters();
+        }
+
+        /// <summary>
+        /// Implementation of GetEncounter
+        /// </summary>
+        /// <param name="id">hte encounter id</param>
+        /// <returns>The encounter found</returns>
+        public Encounter GetEncounter(string id)
+        {
+            return encounters.Where(x => x.ID == id).FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Implementation of GetEncounters
+        /// </summary>
+        /// <returns>A encounter collection</returns>
+        public IEnumerable<Encounter> GetEncounters()
+        {
+            if (encounters == null)
+            {
+                InitEncounters();
+            }
+
+            return encounters;
+        }
+
+        private void InitEncounters()
+        {
+            if (encounters == null)
+            {
+                encounters = new List<Encounter>();
+            }
+            else
+            {
+                encounters.Clear();
+            }
+
+            for (var i = 1; i < 35; i++)
+            {
+                var encounter = new Encounter()
+                {
+                    ID = "R" + i,
+                    Name = "R" + i + ". Sample Encounter",
+                    Adventure = "Runtime Adventure"
+                };
+                encounters.Add(encounter);
+            }
+        }
+    }
+}
